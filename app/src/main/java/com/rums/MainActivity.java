77786316a4b01ceb9bc1a6ea19db21287fb84ebc
ZitaRR <Haeggqvist1998@hotmail.com>
2021-10-05 23:06:04 +0200

    Implemented simple action bar
    - Created the action bar, requires more work
    - Created new string value, containing the welcome message

--- app/src/main/java/com/rums/MainActivity.java
+++ app/src/main/java/com/rums/MainActivity.java
@@ -1,14 +1,18 @@
 package com.rums;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionBar = findViewById(R.id.main_actionbar);
+        setSupportActionBar(actionBar);
     }
 }
