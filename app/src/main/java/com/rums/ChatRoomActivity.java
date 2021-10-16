77559f1c3e2afd1f928653e5860a0216be12ad69
krdevel <91143280+krdevel@users.noreply.github.com>
2021-10-16 18:56:57 +0200

    Scroll to bottom

--- app/src/main/java/com/rums/ChatRoomActivity.java
+++ app/src/main/java/com/rums/ChatRoomActivity.java
@@ -25,14 +25,18 @@ public class ChatRoomActivity extends AppCompatActivity {
 
     private void setupListViewAdapter() {
         names = new ArrayList<>();
-        names.add("Kalle");
-        names.add("Bille");
-        names.add("Mmmmmmmm. Mmmm. Ett längre meddelande som kanske wrappar. Är det så? Det får vi kolla. Ett långt, långt meddelande" +
-                "som fortsätter vidare och vidare och vidare.");
+        int duplicates = 1;
+        for(int i = 0; i<duplicates; i++) {
+            names.add("Kalle");
+            names.add("Bille");
+            names.add("Mmmmmmmm. Mmmm. Ett längre meddelande som kanske wrappar. Är det så? Det får vi kolla. Ett långt, långt meddelande" +
+                    "som fortsätter vidare och vidare och vidare.");
+        }
 
         adapter = new ArrayAdapter<>(this, R.layout.chat_bubble_list_item, names);
         listView = findViewById(R.id.list_view);
         listView.setAdapter(adapter);
-
+        //Scroll to bottom:
+        listView.post(() -> listView.setSelection(listView.getCount() - 1));
     }
 }
