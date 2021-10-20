0de6d5611624a135aaaa50dbaf5a9797bc8a7d30
ehilding <erik.hildingsson@gmail.com>
2021-10-20 22:24:42 +0200

    Fixed navigation from Home to Logout and Edit Profile

--- app/src/main/java/com/rums/HomeActivity.java
+++ app/src/main/java/com/rums/HomeActivity.java
@@ -1,12 +1,17 @@
 package com.rums;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
     private Toolbar actionBar;
@@ -28,6 +33,29 @@ public class HomeActivity extends AppCompatActivity {
     private void startSomeActivity(Class<?> cls) {
         Intent intent = new Intent(this, cls);
         startActivity(intent);
+    }
+
+    // Adding Logout Functionality
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(this, LoginActivity.class));
+                finish();
+                return true;
+            case R.id.menu_profile:
+                startActivity(new Intent(this, UserProfile.class));
+                finish();
+                return true;
+        }
+        return false;
     }
 
 }
