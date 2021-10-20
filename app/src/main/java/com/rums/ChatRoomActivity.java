375948f90c40c4c0abb6e20912f361e77b88ae41
krdevel <91143280+krdevel@users.noreply.github.com>
2021-10-20 11:13:47 +0200

    Med action bar

--- app/src/main/java/com/rums/ChatRoomActivity.java
+++ app/src/main/java/com/rums/ChatRoomActivity.java
@@ -11,6 +11,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import java.util.ArrayList;
 
@@ -20,12 +21,17 @@ public class ChatRoomActivity extends AppCompatActivity {
     ArrayList<String> names;
     ArrayAdapter<String> adapter;
     ListView listView;
+    private Toolbar actionBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);
         setupListViewAdapter();
+
+        actionBar = findViewById(R.id.main_actionbar);
+        setSupportActionBar(actionBar);
 
     }
 
