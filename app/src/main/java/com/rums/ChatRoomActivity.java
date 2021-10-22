327976afd009afee7cbf8c432be47fa677050fc0
krdevel <91143280+krdevel@users.noreply.github.com>
2021-10-22 15:55:01 +0200

    setDisplayHomeAsUpEnabled

--- app/src/main/java/com/rums/ChatRoomActivity.java
+++ app/src/main/java/com/rums/ChatRoomActivity.java
@@ -14,6 +14,7 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -40,6 +41,10 @@ public class ChatRoomActivity extends AppCompatActivity {
 
         actionBar = findViewById(R.id.chat_room_actionbar);
         setSupportActionBar(actionBar);
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
 
     }
 
