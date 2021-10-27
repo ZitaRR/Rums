498d281f088108bc62532815ddbae5109285c56c
ehilding <erik.hildingsson@gmail.com>
2021-10-27 22:03:50 +0200

    Working recyclerview, ugly af with no functionality, hardcoded info

--- app/src/main/java/com/rums/HomeActivity.java
+++ app/src/main/java/com/rums/HomeActivity.java
@@ -3,6 +3,8 @@ package com.rums;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,15 +12,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
     private Toolbar actionBar;
     private Button testActivity;
+    private RecyclerView homeRecycler;
+
+    // Test för att få till recycler bara
+    ArrayList<String> chatRoomNames = new ArrayList<>();
+    ArrayList<String> chatUserNames = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,8 @@ public class HomeActivity extends AppCompatActivity {
 
         actionBar = findViewById(R.id.main_actionbar);
         setSupportActionBar(actionBar);
+
+        homeRecycler = findViewById(R.id.recycler_home);
 
         testActivity = findViewById(R.id.button_test_activity);
 
@@ -37,6 +46,17 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        chatRoomNames.add("ChattOne");
+        chatRoomNames.add("ChattTwooo");
+        chatUserNames.add("Kalle");
+        chatUserNames.add("Johan");
+        HomeAdapter homeAdapter = new HomeAdapter(this, chatRoomNames, chatUserNames);
+        homeRecycler.setAdapter(homeAdapter);
+        homeRecycler.setLayoutManager(new LinearLayoutManager(this));
+
+
+
     }
 
 
---
+++ app/src/main/java/com/rums/HomeAdapter.java
@@ -0,0 +1,63 @@
+package com.rums;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class HomeAdapter extends RecyclerView.Adapter <HomeAdapter.HomeViewHolder> {
+    ArrayList<String> chatRoomNames;
+    ArrayList<String> chatUserNames;
+    Context context;
+
+
+    public HomeAdapter (Context ct, ArrayList<String> roomNames, ArrayList<String> userNames) {
+
+        context = ct;
+        chatRoomNames = roomNames;
+        chatUserNames = userNames;
+
+
+    }
+
+    @NonNull
+    @Override
+    public HomeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.home_chat_rows, parent, false);
+        return new HomeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeViewHolder holder, int position) {
+    holder.myText1.setText(chatRoomNames.get(position));
+    holder.myText2.setText(chatUserNames.get(position));
+    holder.myImg.setImageResource(R.drawable.ic_age);
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatRoomNames.size();
+    }
+
+    public class HomeViewHolder extends RecyclerView.ViewHolder {
+
+        TextView myText1, myText2;
+        ImageView myImg;
+
+        public HomeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myText1 = itemView.findViewById(R.id.textview_chatlist_room);
+            myText2 = itemView.findViewById(R.id.textview_chatlist_username);
+            myImg = itemView.findViewById(R.id.imageview_chatlist_profilepic);
+        }
+    }
+
+}
