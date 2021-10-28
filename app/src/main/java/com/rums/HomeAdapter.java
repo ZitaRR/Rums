e940667ad7157fc46f4c85b9ea4f395ac82ab952
ehilding <erik.hildingsson@gmail.com>
2021-10-28 14:55:49 +0200

    Made rows in recyclerview clickable

--- app/src/main/java/com/rums/HomeAdapter.java
+++ app/src/main/java/com/rums/HomeAdapter.java
@@ -1,6 +1,7 @@
 package com.rums;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -40,6 +42,15 @@ public class HomeAdapter extends RecyclerView.Adapter <HomeAdapter.HomeViewHolde
     holder.myText1.setText(chatRoomNames.get(position));
     holder.myText2.setText(chatUserNames.get(position));
     holder.myImg.setImageResource(R.drawable.ic_age);
+
+    // När man klickar på en rad..
+    holder.homeChatRows.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i = new Intent(context, ChatRoomActivity.class);
+            context.startActivity(i);
+        }
+    });
     }
 
     @Override
@@ -51,12 +62,14 @@ public class HomeAdapter extends RecyclerView.Adapter <HomeAdapter.HomeViewHolde
 
         TextView myText1, myText2;
         ImageView myImg;
+        ConstraintLayout homeChatRows;
 
         public HomeViewHolder(@NonNull View itemView) {
             super(itemView);
             myText1 = itemView.findViewById(R.id.textview_chatlist_room);
             myText2 = itemView.findViewById(R.id.textview_chatlist_username);
             myImg = itemView.findViewById(R.id.imageview_chatlist_profilepic);
+            homeChatRows = itemView.findViewById(R.id.home_chat_rows);
         }
     }
 
