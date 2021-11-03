93901037ec47989cec01d83574d784f0060b3399
ElliFH <helene.fogde.hejda@gmail.com>
2021-11-03 10:57:27 +0100

    .

--- app/src/main/java/com/rums/NewRoomActivity.java
+++ app/src/main/java/com/rums/NewRoomActivity.java
@@ -106,6 +106,8 @@ public class NewRoomActivity extends BaseClassActivity {
                    .setPositiveButton(R.string.say_hello, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
+
+                            // moveToChatRoom ();
 /*
                             for(int i = 0; i <userRow.getChildCount(); i++) {
                             if (checkBox.isChecked()) {
